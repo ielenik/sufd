60c605a35ad4e8c5b0a6bb70f4c2a76281269767
ielenik <ilye@yandex.ru>
2023-03-05 14:02:43 +0300

    make 8 duples

--- src/dataset.py
+++ src/dataset.py
@@ -9,7 +9,7 @@ import math
 import json
 import os
 from tqdm import tqdm
-from random import shuffle
+import random
 from . import config as conf
 
 def load_rkk(path, records):
@@ -70,8 +70,9 @@ def load_dataset(path):
 
     load_rkk(path + '/dataset/spoof', records)
     load_rkk(path + '/dataset/RKK', records)
-
-    shuffle(records)
+    
+    random.seed(123)
+    random.shuffle(records)
     print("Loaded " + str(len(records)) + " records total")
     return records
 
@@ -93,90 +94,101 @@ def getFaceDatasets():
         scalefit = tilesize_full/max(w,h)
         if scalefit > 1:
             scalefit = 1
-        scale = np.random.randint(30,200)/100.*scalefit
 
-        ranshx = max((w*scale-tilesize_full)/2, tilesize_full/8)
-        ranshy = max((h*scale-tilesize_full)/2, tilesize_full/8)
+        masks = []
+        trans = []
+        for i in range(8):
+            scale = np.random.randint(30,200)/100.*scalefit
 
-        angle = math.radians(np.random.randint(-60,60))
-        shx = np.random.randint(-ranshx,ranshx)
-        shy = np.random.randint(-ranshy,ranshy)
+            ranshx = max((w*scale-tilesize_full)/2, tilesize_full/8)
+            ranshy = max((h*scale-tilesize_full)/2, tilesize_full/8)
 
-        if ind < len(records)//part_for_validation:
-            angle = 0
-            shx = 0
-            shy = 0
-            scale = tilesize_full/max(w,h)
-            if scale > 1:
-                scale = 1
+            angle = math.radians(np.random.randint(-60,60))
+            shx = np.random.randint(-ranshx,ranshx)
+            shy = np.random.randint(-ranshy,ranshy)
 
-        tr = matrix([[scale*math.cos(angle),scale*math.sin(angle),tilesize_full/2-scale*(w*math.cos(angle)+h*math.sin(angle))/2 + shx],
-                    [scale*-math.sin(angle),scale*math.cos(angle),tilesize_full/2-scale*(-w*math.sin(angle)+h*math.cos(angle))/2 + shy],
-                    [0,0,1]])
-        Tinv = linalg.inv(tr)
-        Tinvtuple = np.array([Tinv[0,0],Tinv[0,1], Tinv[0,2], Tinv[1,0],Tinv[1,1],Tinv[1,2],0,0]).astype(np.float32)
+            if ind < len(records)//part_for_validation:
+                angle = 0
+                shx = 0
+                shy = 0
+                scale = tilesize_full/max(w,h)
+                if scale > 1:
+                    scale = 1
 
-        mask = np.zeros((tilesize_small,tilesize_small,8)).astype(np.float32)
-        mask[:,:,0] = 1
+            tr = matrix([[scale*math.cos(angle),scale*math.sin(angle),tilesize_full/2-scale*(w*math.cos(angle)+h*math.sin(angle))/2 + shx],
+                        [scale*-math.sin(angle),scale*math.cos(angle),tilesize_full/2-scale*(-w*math.sin(angle)+h*math.cos(angle))/2 + shy],
+                        [0,0,1]])
+            Tinv = linalg.inv(tr)
+            Tinvtuple = np.array([Tinv[0,0],Tinv[0,1], Tinv[0,2], Tinv[1,0],Tinv[1,1],Tinv[1,2],0,0]).astype(np.float32)
 
-        for f in records[ind][1]:
-            rx = (tr[0,0]*f[0]+tr[0,1]*f[1] + tr[0,2])/anchors_scale
-            ry = (tr[1,0]*f[0]+tr[1,1]*f[1] + tr[1,2])/anchors_scale
+            mask = np.zeros((tilesize_small,tilesize_small,8)).astype(np.float32)
+            mask[:,:,0] = 1
 
-            x = int(rx)
-            y = int(ry)
+            for f in records[ind][1]:
+                rx = (tr[0,0]*f[0]+tr[0,1]*f[1] + tr[0,2])/anchors_scale
+                ry = (tr[1,0]*f[0]+tr[1,1]*f[1] + tr[1,2])/anchors_scale
 
-            s = f[2]*scale
-            a = f[3]+angle
+                x = int(rx)
+                y = int(ry)
 
-            ms = int(round(s/4/anchors_scale))
-            sx,ex,sy,ey = x-ms,x+ms+2,y-ms,y+ms+2
-            
-            if sx < 0: sx = 0
-            if sy < 0: sy = 0
-            if ex > tilesize_small: ex = tilesize_small
-            if ey > tilesize_small: ey = tilesize_small
-            
-            if sx >= ex or sy >= ey:
-                continue
+                s = f[2]*scale
+                a = f[3]+angle
 
-            mask[sy:ey,sx:ex,0] = 0
-
-            if s < 8 or s > 48:
-                continue
-            if f[4] < 0.5:
-                continue
-
-            if x < 0 or x >= tilesize_small or y < 0 or y>= tilesize_small:
-                continue
+                ms = int(round(s/4/anchors_scale))
+                sx,ex,sy,ey = x-ms,x+ms+2,y-ms,y+ms+2
                 
-            for i in range(x,x+2):
-                if i < 0 or i >= tilesize_small:
+                if sx < 0: sx = 0
+                if sy < 0: sy = 0
+                if ex > tilesize_small: ex = tilesize_small
+                if ey > tilesize_small: ey = tilesize_small
+                
+                if sx >= ex or sy >= ey:
                     continue
-                for j in range(y,y+2):
-                    if j < 0 or j>=tilesize_small:
+
+                mask[sy:ey,sx:ex,0] = 0
+
+                if s < 8 or s > 48:
+                    continue
+                if f[4] < 0.5:
+                    continue
+
+                if x < 0 or x >= tilesize_small or y < 0 or y>= tilesize_small:
+                    continue
+                    
+                for i in range(x,x+2):
+                    if i < 0 or i >= tilesize_small:
                         continue
-                    mask[j,i,1] = 1
-                    mask[j,i,2] = (rx - i)
-                    mask[j,i,3] = (ry - j)
-                    mask[j,i,4] = s
-                    mask[j,i,5] = a
-                    mask[j,i,6] = f[5]
-                    mask[j,i,7] = f[6]
-        return filename, Tinvtuple, mask
+                    for j in range(y,y+2):
+                        if j < 0 or j>=tilesize_small:
+                            continue
+                        mask[j,i,1] = 1
+                        mask[j,i,2] = (rx - i)
+                        mask[j,i,3] = (ry - j)
+                        mask[j,i,4] = s
+                        mask[j,i,5] = a
+                        mask[j,i,6] = f[5]
+                        mask[j,i,7] = f[6]
+            masks.append(mask)
+            trans.append(Tinvtuple)
+        return filename, np.array(masks), np.array(trans)
 
 
-    def read_image(filename, tr, mask):
+    def read_image(filename, masks, trans):
         image_file = tf.io.read_file(filename, name='read_file')
         image = tf.image.decode_jpeg(image_file, channels=3)
-        tr.set_shape((8))
-        mask.set_shape((tilesize_small,tilesize_small,8))
-        image = tfa.image.transform(image,tr, output_shape=[tilesize_full,tilesize_full], interpolation = 'nearest')
-        image = tf.cast(image,tf.float32)/127.5-1
-        return image, mask
+        batch = []
+#        for tr, mask in params:
+        for i in range(8):
+            tr = trans[i]
+            tr.set_shape((8))
+            masks[i].set_shape((tilesize_small,tilesize_small,8))
+            imaged = tfa.image.transform(image,tr, output_shape=[tilesize_full,tilesize_full], interpolation = 'bilinear')
+            imaged = tf.cast(imaged,tf.float32)/127.5-1
+            batch.append(imaged)
+        return batch, masks
 
 
-    tf_prepare_image_transform = lambda index: tf.numpy_function(prepare_image_transform, [index], [tf.string,tf.float32,tf.float32])
+    tf_prepare_image_transform = lambda index: tf.numpy_function(prepare_image_transform, [index], [tf.string, tf.float32, tf.float32])
 
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
@@ -184,15 +196,15 @@ def getFaceDatasets():
     train = tf.data.Dataset.from_tensor_slices( tf.range(len(records)//part_for_validation,len(records)) )
     train = train.map(tf_prepare_image_transform, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     train = train.map(read_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    train = train.apply(tf.data.experimental.ignore_errors())
+    train = train.unbatch()
+    train = train.apply(tf.data.experimental.ignore_errors()).shuffle(buffer_size = 10 * BATCH_SIZE) 
     train = train.repeat()
     train = train.batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
     train = train.with_options(options)
-
     valid = tf.data.Dataset.from_tensor_slices( tf.range(0, len(records)//part_for_validation) )
-    valid = valid.map(tf_prepare_image_transform, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    valid = valid.map(tf_prepare_image_transform, num_parallel_calls=tf.data.experimental.AUTOTUNE).unbatch()
     valid = valid.map(read_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    valid = valid.apply(tf.data.experimental.ignore_errors())
+    valid = valid.apply(tf.data.experimental.ignore_errors()).shuffle(buffer_size = 10 * BATCH_SIZE) 
     valid = valid.batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
     valid = valid.with_options(options)
 
